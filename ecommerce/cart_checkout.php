245cce094e4371932c753824e03a5cc0ef3e43f9
JessyBoi123 <gultebako@gmail.com>
2022-05-05 14:20:55 +0800

    email sending feature done - by: arjie redona, arjie dbest

--- ecommerce/cart_checkout.php
+++ ecommerce/cart_checkout.php
@@ -6,7 +6,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 if (isset($_POST['saveCart'])) {
 
-    $conn = $pdo->open();
+    
+
+     $conn = $pdo->open();
 
     try {
         $now = date('Y-m-d');
@@ -33,9 +35,9 @@ if (isset($_POST['saveCart'])) {
 
         $salesid = $conn->lastInsertId();
 
-        $_SESSION['success'] = 'Order is now for payment';
+             $_SESSION['success'] = 'Order is now for payment';
 
-        try {
+             try {
             $stmt = $conn->prepare("SELECT * FROM cart LEFT JOIN products ON products.id=cart.product_id WHERE user_id=:user_id");
             $stmt->execute(['user_id' => $user['id']]);
 
@@ -49,42 +51,53 @@ if (isset($_POST['saveCart'])) {
 
             $_SESSION['success'] = 'Transaction successful. Thank you.';
 
-            // //Create a new PHPMailer instance
-            // $mail = new PHPMailer;
+            
+    
+        $name = "Arjie Test [SMPT]";  // Name of your website or yours
+        $to = "tester.emailer10@gmail.com";  // mail of reciever
+        $subject = "Jessie tutawsan lang";
+        $body = "Sending Email Test - If success integrate the code to Jessie's project.";
+        $from = "tester.emailer10@gmail.com";  // you mail
+        $password = "testermail01";  // your mail password
 
-            // //Tell PHPMailer to use SMTP
-            // $mail->isSMTP();
+        // Ignore from here
 
-            // //Set the hostname of the mail server
-            // $mail->Host = 'smtp.gmail.com';
-            // // use
-            // // $mail->Host = gethostbyname('smtp.gmail.com');
-            // // if your network does not support SMTP over IPv6
+        require_once "PHPMailer/src/PHPMailer.php";
+        require_once "PHPMailer/src/SMTP.php";
+        require_once "PHPMailer/src/Exception.php";
+        $mail = new PHPMailer();
 
-            // //Set the SMTP port number - 587 for authenticated TLS
-            // $mail->Port = 587;
+        // To Here
 
-            // //Set the encryption system to use - ssl (deprecated) or tls
-            // $mail->SMTPSecure = 'tls';
+        //SMTP Settings
+        $mail->isSMTP();
+        // $mail->SMTPDebug = 3;  Keep It commented this is used for debugging                          
+        $mail->Host = "smtp.gmail.com"; // smtp address of your email
+        $mail->SMTPAuth = true;
+        $mail->Username = $from;
+        $mail->Password = $password;
+        $mail->Port = 587;  // port
+        $mail->SMTPSecure = "tls";  // tls or ssl
+        $mail->smtpConnect([
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            ]
+        ]);
 
-            // //Whether to use SMTP authentication
-            // $mail->SMTPAuth = true;
-
-            // //Username to use for SMTP authentication - use full email address for gmail
-            // $mail->Username = "gultebako@gmail.com";
-
-            // //Password to use for SMTP authentication
-            // $mail->Password = "123@ld3n!";
-
-            // //Set who the message is to be sent from
-            // $mail->setFrom('gultebako@gmail.com', 'Jessiee jess');
-
-            // // //Set an alternative reply-to address
-            // // $mail->addReplyTo('replyto@example.com', 'First Last');
-
-            // //Set who the message is to be sent to
-            // $mail->addAddress('cruzdenmarvic9@gmail.com', 'Den Cruz');
-            // $mail->send();
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($from, $name);
+        $mail->addAddress($to); // enter email address whom you want to send
+        $mail->Subject = ("$subject");
+        $mail->Body = $body;
+        if ($mail->send()) {
+            echo "Email is sent!";
+        } else {
+            echo "Something is wrong: <br><br>" . $mail->ErrorInfo;
+        } 
+    
 
         } catch (PDOException $e) {
             $_SESSION['error'] = $e->getMessage();
